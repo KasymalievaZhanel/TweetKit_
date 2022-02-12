6308b8f64ece02029e82aa04e6630549200926e3
KasymalievaZhanel <63599290+KasymalievaZhanel@users.noreply.github.com>
2022-02-12 03:42:50 +0300

    Update views.py

--- tweets/views.py
+++ tweets/views.py
@@ -7,7 +7,6 @@ from django.utils.http import is_safe_url
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
-# Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/feed.html")
 
